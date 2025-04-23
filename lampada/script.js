e2fc11ce65cdb6ccc19f932b04f2efbdae25e43e
VitorEduardo7 <161628040+VitorEduardo7@users.noreply.github.com>
2025-04-23 15:20:30 -0300

    Add files via upload

---
+++ lampada/script.js
@@ -0,0 +1,30 @@
+
+const lampada = document.getElementById("lampada");
+const btnLigar = document.getElementById("ligar");
+const btnDesligar = document.getElementById("desligar");
+const estadoTexto = document.getElementById("estadoLampada");
+
+function ligarLampada() {
+ lampada.src = "acesa.jpg";
+ lampada.alt = "Lâmpada Ligada";
+}
+function desligarLampada() {
+ lampada.src = "apagada.jpg";
+ lampada.alt = "Lâmpada Desligada";
+}
+
+btnLigar.addEventListener("click", ligarLampada);
+btnDesligar.addEventListener("click", desligarLampada);
+
+function ligarLampada() {
+    lampada.src = "acesa.jpg";
+    lampada.alt = "Lâmpada Ligada";
+    estadoTexto.innerText = "A lâmpada está ligada";
+  }
+  
+  function desligarLampada() {
+    lampada.src = "apagada.jpg";
+    lampada.alt = "Lâmpada Desligada";
+    estadoTexto.innerText = "A lâmpada está desligada";
+  }
+  
